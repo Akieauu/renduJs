6169c53d1c841fdf22449beec03f5d504d3c09e3
Akieauu <ILYES.ADLIROCA@latoile.em-lyon.com>
2025-12-17 10:45:45 +0100

    updated

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let urlChocolat = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/chocolatier.json";
 
+const hero = document.getElementsByClassName("section-hero");
+const why = document.getElementsByClassName("section-why");
+const collection = document.getElementsByClassName("section-collection");
+const services = document.getElementsByClassName("section-services");
+
 
 fetch (urlChocolat)
     .then(response => response.json())
@@ -8,6 +13,22 @@ fetch (urlChocolat)
         console.log(data);
         
 
+        let divHero = document.createElement("div");
+        divHero.classList.add("div-hero");
+
+        let titleSectionHero = document.createElement("h1");
+        titleSectionHero.classList.add("title-section-hero");
+        titleSectionHero.textContent = data.nomEntreprise;
+
+        let paraSectionHero = document.createElement("p");
+        paraSectionHero.classList.add("para-section-hero");
+        paraSectionHero.textContent = data.slogan;
+
+        let 
+
+
+
+
         data.produits.forEach(produit => {
             
         });
@@ -15,8 +36,3 @@ fetch (urlChocolat)
     .catch(error => console.error("ntm sa marsh pa: ", error));
 
 
-
-const hero = document.getElementsByClassName("section-hero");
-const why = document.getElementsByClassName("section-why");
-const collection = document.getElementsByClassName("section-collection");
-const services = document.getElementsByClassName("section-services");
